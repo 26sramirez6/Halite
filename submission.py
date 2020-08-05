278d82df39f6e04c99994495c97ff8ebd9cfb601
saul.ramirez <saul.ramirez@transmarketgroup.com>
2020-08-05 16:23:20 -0500

    a

--- submission.py
+++ submission.py
@@ -92,7 +92,7 @@ ships_spawned = 0
 shipyards_spawned = 0
 halite_lost = 0
 halite_stolen = 0
- 
+
 # criterion = nn.CrossEntropyLoss()
 criterion = nn.SmoothL1Loss()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -102,6 +102,97 @@ optimizer = torch.optim.SGD(
     lr=LEARNING_RATE, momentum=MOMENTUM, 
     weight_decay=WEIGHT_DECAY)
 
+class ModelPropagator:
+    def __init__(self, board_size, player_count):
+        self._state = np.zeros(ftr_count)
+        self._ship_halite_prev = np.zeros(player_count)
+        self._ship_halite_cur = np.zeros(player_count)
+        # assume a max amount of ships and shipyards to
+        # so we can allocate upfront
+        self._ship_actions = np.zeros(board_size**2, dtype=np.int32)
+        self._shipyard_actions = np.zeros(board_size**2, dtype=np.int32)
+        self._best_ship_actions = np.zeros(board_size**2, dtype=np.int32)
+        self._best_shipyard_actions = np.zeros(board_size**2, dtype=np.int32)
+        
+    def set_ship_halite_prev(self, ship_halite_prev):
+        self._ship_halite_prev[:] = ship_halite_prev
+        
+    def select_action(self, board, model):
+        ship_count = len(board.current_player.ships)
+        shipyard_count = len(board.current_player.shipyards)
+        action_space = (len(SHIP_ACTIONS)**ship_count) * (len(SHIPYARD_ACTIONS)**shipyard_count)
+        current_halite = board.current_player.halite
+        max_q_value = float('-inf')
+        if action_space > MAX_ACTION_SPACE:
+            # choose a random ship, find the best action for this ship while holding the rest at 0
+            # then choose another ship, holding the previous ship at it's best action. 
+            # continue for all ships
+            self._ship_actions.fill(0)
+            self._shipyard_actions.fill(0)
+            ship_idxs = list(range(ship_count))
+            shipyard_idxs = list(range(shipyard_count))
+            np.random.shuffle(ship_idxs)
+            np.random.shuffle(shipyard_idxs)
+            for i in ship_idxs:
+                for j, _ in enumerate(SHIP_ACTIONS):
+                    self._ship_actions[i] = j
+                
+                    new_board = step_forward(board, self._ship_actions, self._shipyard_actions)
+                    update_state(self._state, new_board, self._ship_halite_cur, self._ship_halite_prev)
+                    q_value = model(self._state)
+                    if q_value > max_q_value:
+                        max_q_value = q_value
+                        self._best_ship_actions[:ship_count] = self._ship_actions[:ship_count]
+                        self._best_shipyard_actions[:shipyard_count] = self._shipyard_actions[:shipyard_count]
+                        
+                self._ship_actions[i] = self._best_ship_actions[i]
+                
+            for i in shipyard_idxs:
+                for j, _ in enumerate(SHIPYARD_ACTIONS):
+                    self._shipyard_actions[i] = j
+                
+                    new_board = step_forward(board, self._ship_actions, self._shipyard_actions)
+                    update_state(self._state, new_board, self._ship_halite_cur, self._ship_halite_prev)
+                    q_value = model(self._state)
+                    if q_value > max_q_value:
+                        max_q_value = q_value
+                        self._best_ship_actions[:ship_count] = self._ship_actions[:ship_count]
+                        self._best_shipyard_actions[:shipyard_count] = self._shipyard_actions[:shipyard_count]
+                        
+                self._shipyard_actions[i] = self._best_shipyard_actions[i]
+        else:
+            for l in product(range(len(SHIP_ACTIONS)), repeat=ship_count):
+                ship_halite = current_halite // 2
+                for y in product(range(len(SHIPYARD_ACTIONS)), repeat=shipyard_count):
+                    shipyard_halite = current_halite // 2
+                    for i, j in enumerate(l):
+                        if SHIP_ACTIONS[j]==ShipAction.CONVERT:
+                            if ship_halite < board.configuration.convert_cost:
+                                j = 0 # set to none action
+                            else:
+                                ship_halite -= board.configuration.convert_cost
+                                
+                        self._ship_actions[i] = j
+                    for i, j in enumerate(y):
+                        if SHIPYARD_ACTIONS[j]==ShipyardAction.SPAWN:
+                            if shipyard_halite < board.configuration.spawn_cost:
+                                j = 0 # set to none
+                            else
+                                shipyard_halite -= board.configuration.spawn_cost
+                        self._shipyard_actions[i] = j
+                    
+                    new_board = step_forward(board, self._ship_actions, self._shipyard_actions)
+                    update_state(self._state, new_board, self._ship_halite_cur, self._ship_halite_prev)
+                    q_value = model(self._state)
+                    if q_value > max_q_value:
+                        max_q_value = q_value
+                        self._best_ship_actions[:ship_count] = self._ship_actions[:ship_count]
+                        self._best_shipyard_actions[:shipyard_count] = self._shipyard_actions[:shipyard_count]
+        
+        set_next_actions(board, self._best_ship_actions, self._best_shipyard_actions)
+        
+mp = ModelPropagator(PLAYERS)
+    
 def train(model, device, training, optimizer, epoch, criterion):
     model.train()
     training = torch.from_numpy(training).float()
@@ -183,211 +274,83 @@ def new_position(size, ship, action):
         
     return ret
 
-class ExampleCreator:
-    def __init__(self, ftr_count, device):
-        self._example = torch.zeros(ftr_count, dtype=torch.int32, device=device)
-        
-    def create_example(self, board, ship_actions, shipyard_actions):
-        self._example[:halite_ftr] = board.observation["halite"]
-        current_player = board.current_player
-        for i, my_ship in enumerate(current_player.ships):
-            pos = my_ship.position
-            
-            if ship_actions[i]==0:
-                next_action = None if ship_actions[i]==0 else ShipAction(ship_actions[i])
-            else:
-                next_action = ShipAction(ship_actions[i])
-            next_pos = new_position(BOARD_SIZE, my_ship, next_action)
-            ship_index = (next_pos.y)*BOARD_SIZE + next_pos.x
-            self._example[halite_ftr + ship_index] = 1
-            # reduce the halite farmed by 25% if we didn't move
-            if next_action is None:
-                halite_mined = self._example[:halite_ftr][ship_index] * board.configuration.collect_rate
-                self._example[halite_ftr + my_ship_exist_ftr + ship_index] += halite_mined
-                self._example[:halite_ftr][ship_index] -= halite_mined
-            else:
-                self._example[halite_ftr + my_ship_exist_ftr + ship_index] = my_ship.halite 
-            
-#             ship_halite_cur[0] += my_ship.halite
-        for i, my_shipyard in enumerate(current_player.shipyards):
-            pos = my_shipyard.position
-            next_action = ShipyardAction(shipyard_actions[i])
-            if shipyard_action 
-            shipyard_index = (pos.y)*BOARD_SIZE + pos.x
-            episodes[board.step, halite_ftr + my_ship_exist_ftr + my_ship_halite_ftr + shipyard_index] = 1
-            
-        ftr_index = halite_ftr + my_ship_exist_ftr + my_ship_halite_ftr + my_shipyard_exist_ftr
-        for i, player in enumerate(board.opponents):
-            for enemy_ship in player.ships:
-                pos = enemy_ship.position
-                ship_index = (pos.y)*BOARD_SIZE + pos.x
-                episodes[board.step, ftr_index + ship_index] = 1
-                episodes[board.step, ftr_index + enemy_ship_exist_ftr + ship_index] = enemy_ship.halite
-                ship_halite_cur[i+1] += enemy_ship.halite
-            for enemy_shipyard in player.shipyards:
-                pos = enemy_shipyard.position
-                shipyard_index = (pos.y)*BOARD_SIZE + pos.x
-                episodes[board.step, ftr_index + enemy_ship_exist_ftr + enemy_ship_halite_ftr + shipyard_index] = 1
-        
-        ftr_index += enemy_ship_exist_ftr + enemy_ship_halite_ftr + enemy_shipyard_exist_ftr
-        episodes[board.step, ftr_index] = board.configuration.episode_steps - board.step
-        ftr_index += 1
-        for i, player in enumerate(board.players.values()):
-            episodes[board.step, ftr_index + i] = player.halite
-        ftr_index += halite_deposited_by_player_ftr
-        
-        episodes[board.step, -halite_mined_by_player_ftr:] = np.maximum(player_zeros, ship_halite_cur - ship_halite_prev)
-        ship_halite_prev[:] = ship_halite_cur
-        episode_rewards[board.step] = compute_reward(board)
-        print(board.step)
-        
-        return self._example
-    
+def min_max_norm(arr):
+    arr[:] = 2 * ((arr - arr[0]) / (arr[-1] - arr[0])) - 1
 
-class StateCreator:
-    
-    def step_forward(self, board, ship_actions, shipyard_actions):
-        cp = board.current_player
-        for i, my_ship in enumerate(cp.ships):
-            my_ship.next_action = None if ship_actions[i]==0 else ShipAction(ship_actions[i]) 
-        for i, my_shipyard in enumerate(cp.shipyards):
-            my_shipyard.next_action = None if shipyard_actions[i]==0 else ShipyardAction(ship_actions[i]) 
-        new_board = board.next()
-        return new_board
-    
-    def create_state(self, state, board, ship_actions, shipyard_actions):
-        new_board = self.step_forward(board, ship_actions, shipyard_actions)
-        
-        cp = new_board.current_player
-        state[:halite_ftr] = new_board.observation["halite"]
-        ship_halite_cur.fill(0)
-        
-        for my_ship in cp.ships:
-            pos = my_ship.position
-            ship_index = (pos.y)*BOARD_SIZE + pos.x
-            state[ halite_ftr + ship_index] = 1
-            state[ halite_ftr + my_ship_exist_ftr + ship_index] = my_ship.halite
-            ship_halite_cur[0] += my_ship.halite
-        for my_shipyard in cp.shipyards:
-            pos = my_shipyard.position
-            shipyard_index = (pos.y)*BOARD_SIZE + pos.x
-            state[ halite_ftr + my_ship_exist_ftr + my_ship_halite_ftr + shipyard_index] = 1
-            
-        ftr_index = halite_ftr + my_ship_exist_ftr + my_ship_halite_ftr + my_shipyard_exist_ftr
-        for i, player in enumerate(new_board.opponents):
-            for enemy_ship in player.ships:
-                pos = enemy_ship.position
-                ship_index = (pos.y)*BOARD_SIZE + pos.x
-                state[ ftr_index + ship_index] = 1
-                state[ ftr_index + enemy_ship_exist_ftr + ship_index] = enemy_ship.halite
-                ship_halite_cur[i+1] += enemy_ship.halite
-            for enemy_shipyard in player.shipyards:
-                pos = enemy_shipyard.position
-                shipyard_index = (pos.y)*BOARD_SIZE + pos.x
-                state[ ftr_index + enemy_ship_exist_ftr + enemy_ship_halite_ftr + shipyard_index] = 1
-        
-        ftr_index += enemy_ship_exist_ftr + enemy_ship_halite_ftr + enemy_shipyard_exist_ftr
-        state[ftr_index] = new_board.configuration.episode_steps - new_board.step
-        ftr_index += 1
-        for i, player in enumerate(new_board.players.values()):
-            state[ftr_index + i] = player.halite
-        ftr_index += halite_deposited_by_player_ftr
-        
-        state[-halite_mined_by_player_ftr:] = np.maximum(player_zeros, ship_halite_cur - ship_halite_prev)
-        ship_halite_prev[:] = ship_halite_cur
-        
-        return
+def set_next_actions(board, ship_actions, shipyard_actions):
+    cp = board.current_player
+    for i, my_ship in enumerate(cp.ships):
+        my_ship.next_action = None if ship_actions[i]==0 else ShipAction(ship_actions[i]) 
+    for i, my_shipyard in enumerate(cp.shipyards):
+        my_shipyard.next_action = None if shipyard_actions[i]==0 else ShipyardAction(ship_actions[i]) 
+          
+def step_forward(board, ship_actions, shipyard_actions):
+    set_next_actions(board, ship_actions, shipyard_actions)
+    new_board = board.next()
+    return new_board
 
-        
-class ActionSelector:
-    def __init__(self, example_creator):
-        self._example_creator = example_creator
-        
-    def model_select_action(self, board, model):
-        action_space = (len(SHIP_ACTIONS)**len(board.current_player.ships)) * (len(SHIPYARD_ACTIONS)**len(board.current_player.shipyards))
-        current_halite = board.current_player.halite
-        ship_actions = np.zeros(len(board.current_player.ships), dtype=np.int32)
-        shipyard_actions = np.zeros(len(board.current_player.ships), dtype=np.int32)
-        if action_space > MAX_ACTION_SPACE:
-            ship_idx = np.random.randn()
-            np.random.randint(len(board.current_player.ships))
-        else:
-            for l in product(range(len(SHIP_ACTIONS)), repeat=len(board.current_player.ships)):
-                ship_halite = current_halite // 2
-                ship_actions.fill(0)
-                shipyard_actions.fill(0)
-                for y in product(range(len(SHIPYARD_ACTIONS)), repeat=len(board.current_player.shipyards)):
-                    shipyard_halite = current_halite // 2
-                    for i, j in enumerate(l):
-                        if SHIP_ACTIONS[j]==ShipAction.CONVERT:
-                            if ship_halite < board.configuration.convert_cost:
-                                j = 0 # set to none action
-                            else:
-                                ship_halite -= board.configuration.convert_cost
-                                
-                        ship_actions[i] = j
-                    for i, j in enumerate(y):
-                        if SHIPYARD_ACTIONS[j]==ShipyardAction.SPAWN:
-                            if shipyard_halite < board.configuration.spawn_cost:
-                                j = 0 # set to none
-                            else
-                                shipyard_halite -= board.configuration.spawn_cost
-                        shipyard_actions[i] = j
-                    example = self._example_creator.create_example(board, ship_actions, shipyard_actions)
-                    q = model(example)       
-                 
-        return
-
-    
-def agent(obs, config):
-    board = Board(obs, config)
-    current_player = board.current_player
-    episodes[board.step, :halite_ftr] = board.observation["halite"]
+def update_state(state, board, ship_halite_cur, ship_halite_prev):        
+    cp = board.current_player
+    state[:halite_ftr] = board.observation["halite"]
     ship_halite_cur.fill(0)
     
-    for my_ship in current_player.ships:
+    for my_ship in cp.ships:
         pos = my_ship.position
         ship_index = (pos.y)*BOARD_SIZE + pos.x
-        episodes[board.step, halite_ftr + ship_index] = 1
-        episodes[board.step, halite_ftr + my_ship_exist_ftr + ship_index] = my_ship.halite
+        state[ halite_ftr + ship_index] = 1
+        state[ halite_ftr + my_ship_exist_ftr + ship_index] = my_ship.halite
         ship_halite_cur[0] += my_ship.halite
-    for my_shipyard in current_player.shipyards:
+    for my_shipyard in cp.shipyards:
         pos = my_shipyard.position
         shipyard_index = (pos.y)*BOARD_SIZE + pos.x
-        episodes[board.step, halite_ftr + my_ship_exist_ftr + my_ship_halite_ftr + shipyard_index] = 1
+        state[ halite_ftr + my_ship_exist_ftr + my_ship_halite_ftr + shipyard_index] = 1
         
     ftr_index = halite_ftr + my_ship_exist_ftr + my_ship_halite_ftr + my_shipyard_exist_ftr
     for i, player in enumerate(board.opponents):
         for enemy_ship in player.ships:
             pos = enemy_ship.position
             ship_index = (pos.y)*BOARD_SIZE + pos.x
-            episodes[board.step, ftr_index + ship_index] = 1
-            episodes[board.step, ftr_index + enemy_ship_exist_ftr + ship_index] = enemy_ship.halite
+            state[ ftr_index + ship_index] = 1
+            state[ ftr_index + enemy_ship_exist_ftr + ship_index] = enemy_ship.halite
             ship_halite_cur[i+1] += enemy_ship.halite
         for enemy_shipyard in player.shipyards:
             pos = enemy_shipyard.position
             shipyard_index = (pos.y)*BOARD_SIZE + pos.x
-            episodes[board.step, ftr_index + enemy_ship_exist_ftr + enemy_ship_halite_ftr + shipyard_index] = 1
+            state[ ftr_index + enemy_ship_exist_ftr + enemy_ship_halite_ftr + shipyard_index] = 1
     
     ftr_index += enemy_ship_exist_ftr + enemy_ship_halite_ftr + enemy_shipyard_exist_ftr
-    episodes[board.step, ftr_index] = board.configuration.episode_steps - board.step
+    state[ftr_index] = board.configuration.episode_steps - board.step
     ftr_index += 1
+    # the halite score changes based on board configuration. Probably best to
+    # not use the current score and instead use the amount deposited by
+    # each of the players in the previous round? or apply min-max normalization
+    # to create a better signal.
     for i, player in enumerate(board.players.values()):
-        episodes[board.step, ftr_index + i] = player.halite
+        state[ftr_index + i] = player.halite
+    min_max_norm(state[ftr_index: ftr_index+halite_deposited_by_player_ftr])
     ftr_index += halite_deposited_by_player_ftr
     
-    episodes[board.step, -halite_mined_by_player_ftr:] = np.maximum(player_zeros, ship_halite_cur - ship_halite_prev)
-    ship_halite_prev[:] = ship_halite_cur
-    episode_rewards[board.step] = compute_reward(board)
-    print(board.step)
+    # should we also min_max_norm the amount of halite mined?
+    state[-halite_mined_by_player_ftr:] = np.maximum(player_zeros, ship_halite_cur - ship_halite_prev)
+    min_max_norm(state[-halite_mined_by_player_ftr:])
     
-    randomize = np.random.rand() < EGREEDY
-    if randomize:
+    return
+
+    
+def agent(obs, config):
+    board = Board(obs, config)
+    update_state(episodes[board.step], board, ship_halite_cur, ship_halite_prev)
+    ship_halite_prev[:] = ship_halite_cur
+    mp.set_ship_halite_prev(ship_halite_cur)
+    episode_rewards[board.step] = compute_reward(board)
+    print(board.step, episode_rewards[board.step])
+    
+    if np.random.rand() < EGREEDY:
         randomize_action(board)
     else:
-        model_select_action(board, net)
+        mp.select_action(board, net)
         
-    return current_player.next_actions
+    return board.current_player.next_actions
     
 env = make("halite", configuration={"size": BOARD_SIZE, "startingHalite": STARTING, "episodeSteps": EPISODE_STEPS})
 env.reset(PLAYERS)
