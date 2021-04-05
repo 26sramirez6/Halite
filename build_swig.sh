6b8215cfe9b5c4e8179d090327b12b5d520ee0f7
saul.ramirez <saul.ramirez@transmarketgroup.com>
2021-04-04 22:23:04 -0500

    a

--- build_swig.sh
+++ build_swig.sh
@@ -1,4 +1,4 @@
-rm -rf swig_wrap.o halite_swig.so swig_wrap.cxx
-swig -c++ -python swig.i
-/usr/local/bin/c++ -fPIC -I/home/saul.ramirez/anaconda3/include/python3.7m -I/home/saul.ramirez/eclipse-workspace/Halite -c swig_wrap.cxx
-/usr/local/bin/c++ -fPIC -shared swig_wrap.o -o halite_swig.so
+rm -rf halite_swig_wrap.o _halite_swig.so halite_swig_wrap.cxx
+swig -c++ -python halite_swig.i
+/usr/local/bin/c++ -fPIC -I/home/saul.ramirez/anaconda3/include/python3.7m -I/home/saul.ramirez/eclipse-workspace/Halite -c halite_swig_wrap.cxx
+/usr/local/bin/c++ -fPIC -shared halite_swig_wrap.o -o _halite_swig.so
